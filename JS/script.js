c8265d90dbe8c3d2ee0c472df965e35da7e7a3b4
Sara Adel <sarahadel3476@gmail.com>
2024-12-23 09:16:52 +0200

    task

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+// function insertionSort(arr) {
+//     let n = arr.length;
+    
+//     for (let i = 1; i < n; i++) {
+//     let key = arr[i];
+//     let j = i - 1;
+    
+//     // Move elements larger than the key
+//     while (j >= 0 && arr[j] > key) {
+    
+//     arr[j + 1] = arr[j];
+//     j = j - 1;
+//     }
+    
+//     // Put the key in place
+//     arr[j + 1] = key;
+//     }
+//     return arr;
+//     }
+    
+//     let numbers = [8, 4, 6, 2, 9];
+//     console.log(insertionSort(numbers)); // [2, 4, 6, 8, 9]
+
